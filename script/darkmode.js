277a440fd6e25a40c9ed24d9d4fac8f4c5ba8bdd
Jack Perez <39219195+jmprz@users.noreply.github.com>
2022-12-19 03:43:58 +0800

    Update Dark Mode (Version 2.0)
    - New dark mode JavaScript

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,27 +1,32 @@
-/* Dark Mode */
-var darkMode = false;
+var checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
 
-// default to system setting
-if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-	darkMode = true;
+//check storage if dark mode was on or off
+if (sessionStorage.getItem("mode") == "dark") {
+  darkmode(); //if dark mode was on, run this funtion
+} else {
+  nodark(); //else run this funtion
 }
 
-// preference from localStorage should overwrite
-if (localStorage.getItem('theme') === 'dark-mode') {
-	darkMode = true;
-} else if (localStorage.getItem('theme') === 'light') {
-	darkMode = false;
-}
-
-if (darkMode) {
-	document.body.classList.toggle('dark-mode');
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.getElementById('darkmode').addEventListener('click', () => {
-		document.body.classList.toggle('dark-mode');
-    	localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light');
-	});
-
+//if the checkbox state is changed, run a funtion
+checkbox.addEventListener("change", function() {
+  //check if the checkbox is checked or not
+  if (checkbox.checked) {
+    darkmode(); //if the checkbox is checked, run this funtion
+  } else {
+    nodark(); //else run this funtion
+  }
 });
+
+//function for checkbox when checkbox is checked
+function darkmode() {
+  document.body.classList.add("dark-mode"); //add a class to the body tag
+  checkbox.checked = true; //set checkbox to be checked state
+  sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
+}
+
+//function for checkbox when checkbox is not checked
+function nodark() {
+  document.body.classList.remove("dark-mode"); //remove added class from body tag
+  checkbox.checked = false; //set checkbox to be unchecked state
+  sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
+}
